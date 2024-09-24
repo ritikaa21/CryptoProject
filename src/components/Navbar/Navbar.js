f90135dce1a80a87394494edd5489ca62f766cda
Srivastava <2153416@cognizant.com>
2024-09-24 14:52:46 +0530

    Updated all project files

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrown_icon from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const {setCurrency} = useContext(CoinContext)
+
+  const currencyHandler = (event)=>{
+    switch(event.target.value){
+      case "usd": {
+        setCurrency({name: "usd", symbol: "$"})
+        break;
+      }
+      case "inr": {
+        setCurrency({name: "inr", symbol: "₹"})
+        break;
+      }
+      case "eur": {
+        setCurrency({name: "eur", symbol: "€"})
+        break;
+      }
+      default: {
+        setCurrency({name: "usd", symbol: "$"})
+        break;
+      }
+    }
+    
+  }
   return (
     <div className='navbar'>
+      <Link to={`/`}>
         <img src={logo} alt='' className='logo'/>
+      </Link>
         <ul>
-            <li>Home</li>
+            <Link to={`/`}><li>Home</li></Link>
             <li>Features</li>
             <li>Pricing</li>
             <li>Blog</li>
         </ul>
         <div className='nav-right'>
-            <select>
+            <select onChange={currencyHandler}>
                 <option value="usd">USD</option>
                 <option value="inr">INR</option>
                 <option value="eur">EUR</option>
